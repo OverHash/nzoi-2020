23cc2a170fb820cee8957a53a37af292ea3ad348
OverHash <46231745+OverHash@users.noreply.github.com>
2021-04-27 23:32:45 +1200

    Speed up code

--- problems/03.Lineup/src/main.rs
+++ problems/03.Lineup/src/main.rs
@@ -12,61 +12,27 @@ fn main() {
         .split_whitespace()
         .map(|s| s.parse::<u32>().unwrap())
         .collect();
+    our_lineup.sort_unstable();
 
     // read their lineup
     lineup_buff = String::new();
     io::stdin().lock().read_line(&mut lineup_buff).unwrap();
 
-    let their_lineup: Vec<u32> = lineup_buff
+    let mut their_lineup: Vec<u32> = lineup_buff
         .split_whitespace()
         .map(|s| s.parse::<u32>().unwrap())
         .collect();
+    their_lineup.sort_unstable();
 
     // find our optimal layout
-    let mut optimal_layout: Vec<u32> = Vec::with_capacity(our_lineup.len());
 
-    for their_player in &their_lineup {
-        let mut closest_rank = u32::MAX;
-        let mut closest_rank_index: Option<usize> = None;
+    let mut matches_won: u32 = 0;
 
-        for (i, our_player) in our_lineup.iter().enumerate() {
-            if our_player < &closest_rank && our_player > their_player {
-                closest_rank = *our_player;
-                closest_rank_index = Some(i);
-            }
+    for our_player in &our_lineup {
+        if our_player > &their_lineup[matches_won as usize] {
+            matches_won += 1;
         }
-
-        let best_rank_index = match closest_rank_index {
-            Some(v) => v,
-            None => {
-                let mut lowest_num = our_lineup[0];
-                let mut lowest_num_index: usize = 0;
-
-                for (i, player) in our_lineup.iter().skip(1).enumerate() {
-                    if player < &lowest_num {
-                        lowest_num = *player;
-                        lowest_num_index = i;
-                    }
-                }
-
-                lowest_num_index
-            }
-        };
-
-        optimal_layout.push(our_lineup[best_rank_index]);
-        our_lineup.swap_remove(best_rank_index);
     }
-
-    let matches_won = optimal_layout
-        .iter()
-        .enumerate()
-        .fold(0, |acc: u32, (index, our_player)| {
-            if our_player > &their_lineup[index] {
-                return acc + 1;
-            }
-
-            acc
-        });
 
     println!("{}", matches_won);
 }
